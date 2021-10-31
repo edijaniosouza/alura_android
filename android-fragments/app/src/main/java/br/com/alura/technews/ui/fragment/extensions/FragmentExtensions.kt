056bef7d05337a50c522d8d487c46ab833e975d5
edijanio <edijanio.souza@hotmail.com>
2021-10-31 14:07:57 -0300

    first fragment

---
+++ android-fragments/app/src/main/java/br/com/alura/technews/ui/fragment/extensions/FragmentExtensions.kt
@@ -0,0 +1,10 @@
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.mostraErro(mensagem: String) {
+    Toast.makeText(
+        context,
+        mensagem,
+        Toast.LENGTH_LONG
+    ).show()
+}
